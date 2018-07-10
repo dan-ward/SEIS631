224d18d31f6d77e08861b2f275bf900a602952b9
dan-ward <ward1516@stthomas.edu>
2018-07-10 14:27:21 -0500

    Completed Assignment 8

--- Assignment8/Assignment8.R
+++ Assignment8/Assignment8.R
@@ -31,7 +31,17 @@ m2 <- lm(runs ~ homeruns, data = mlb11)
 #display the summary of m1
 summary(m2)
 
-#plot a scatterplot with teh least squares line laid on top
+#plot a scatterplot with the least squares line laid on top
 plot(mlb11$runs ~ mlb11$at_bats)
 abline(m1, col='red')
+
+#plot residuals and at_bats
+plot(m1$residuals ~ mlb11$at_bats)
+abline(h = 0, lty = 3) # adds a horizontal dashed line at y = 0
+
+#Check histogram
+hist(m1$residuals)
+
+qqnorm(m1$residuals)
+qqline(m1$residuals) # adds diagonal line to the normal prob plot
 
